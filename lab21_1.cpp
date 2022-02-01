f533649ee46c11b9ea9def3a9b597e6bfd0ee6a8
NeZE <boss-NeZE@hotmail.com>
2022-02-01 21:54:37 +0700

    1

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
 
+double overlap(Rect R1,Rect R2){
+	double left=max(R1.x, R2.x);
+	double right=min(R1.x+R1.w, R2.x+R2.w);
+	double bottom=max(R1.y-R1.h, R2.y-R2.h);
+	double top=min(R2.y, R1.y);
+	double area;
+	if ( right > left && top > bottom ) return area = (right-left)*(top-bottom);
+	else return 0;
 
+	return 0;
 
 }
